651bf81ae79d9615979f38c5c79bdf5a29745402
Christopher Turner <cturner@craveoncampus.com>
2016-09-17 17:05:56 -0400

    5:05 pm push

---
+++ platforms/ios/HackMIT2016mobile/app/main-page.js
@@ -0,0 +1,26 @@
+var sliderModule = require("ui/slider");
+var createViewModel = require("./main-view-model").createViewModel;
+var observableModule = require("data/observable");
+
+function onNavigatingTo(args) {
+    var page = args.object;
+    page.bindingContext = createViewModel();
+
+    var slider = new sliderModule.Slider();
+    slider.maxValue = 1000;
+    slider.value = 500;
+    slider.minValue = 0;
+    var model = new observableModule.Observable();
+    model.set("confidence", 750);
+    console.log();
+    var options = {
+        sourceProperty: "value",
+        targetProperty: "confidence",
+        twoWay: true
+    };
+    slider.bind(options, model);
+    model.set("confidence", 750);
+    console.log();
+
+}
+exports.onNavigatingTo = onNavigatingTo;
---
+++ platforms/ios/build/HackMIT2016mobile.build/Debug-iphonesimulator/HackMIT2016mobile.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/main.m \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/christopherturner/DevOps/HackMIT2016.mobile/platforms/ios/internal/TNSDebugging.h
